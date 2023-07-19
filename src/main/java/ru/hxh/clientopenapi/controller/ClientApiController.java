74b631f375dea794909f26be013f17472b585d56
Андрей Славкин <slavkin18@yandex.ru>
2023-07-19 19:16:26 +0300

    Добавил интерфейс для сервиса, рефактор методов сервиса и контроллера

--- src/main/java/ru/hxh/clientopenapi/controller/ClientApiController.java
+++ src/main/java/ru/hxh/clientopenapi/controller/ClientApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.hxh.clientopenapi.api.ClientsApi;
 import ru.hxh.clientopenapi.api.model.Client;
 import ru.hxh.clientopenapi.api.model.ClientDTO;
-import ru.hxh.clientopenapi.service.ClientApiService;
+import ru.hxh.clientopenapi.service.Impl.ClientApiServiceImpl;
 
 import java.util.List;
 
@@ -14,7 +14,7 @@ import java.util.List;
 public class ClientApiController implements ClientsApi {
 
     @Autowired
-    ClientApiService clientApiService;
+    ClientApiServiceImpl clientApiService;
 
 
     @Override
@@ -24,7 +24,8 @@ public class ClientApiController implements ClientsApi {
 
     @Override
     public ResponseEntity<Void> createClient(ClientDTO client) {
-        return ResponseEntity.ok(clientApiService.create(client));
+        clientApiService.create(client);
+        return ResponseEntity.ok().build();
     }
 
     @Override
